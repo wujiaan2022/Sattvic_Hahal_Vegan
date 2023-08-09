63a014fb7c6418d6ce218d3fc1e324f0d4abdac6
wujiaan2022 <emmawu2004@gmail.com>
2023-08-09 15:14:46 +0000

    try to fix grid cell image change function error

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,28 +1,27 @@
-// List of image URLs
-const gallery1_ImgChangeUrls = [
-    colorfulDumplings0.jpg,
-    colorfulDumplings1.jpg,
-    colorfulDumplings2.PNG,
-    colorfulDumplings3.PNG,
-    colorfulDumplings4.jpeg,
-    colorfulDumplings5.jpeg,
-    colorfulDumplings6.jpg,
-    colorfulDumplings7.jpg,
-    colorfulDumplings8.jpg,
+var i = 0;
+var imagesGallery1 = [
+    "{% static 'images/colorfulDumplings0.jpg' %}",
+    "{% static 'images/colorfulDumplings1.jpg' %}",
+    "{% static 'images/colorfulDumplings2.PNG.jpg' %}",
 ];
 
-// Get a reference to the grid cell element
-const gallery1_bg = document.getElementById('gallery1-imgChange');
+var slideTime = 3000; // 3 seconds
 
-// Function to change the background image
-function gallery1_ImgChange() {
-    const randomIndex = Math.floor(Math.random() * imageUrls.length);
-    const randomImageUrl = imageUrls[randomIndex];
-    gallery1-imgChange.style.backgroundImage = `url(${randomImageUrl})`;
+function changePicture(imagesGalleryArray, galleryId) {
+    var gallery = document.getElementById(galleryId);
+    
+    gallery.style.backgroundImage = "url(" + imagesGalleryArray[i] + ")";  // Use imagesGalleryArray
+    
+    if (i < imagesGalleryArray.length - 1) {  // Use imagesGalleryArray.length
+        i++;
+    } else {
+        i = 0;
+    }
+    setTimeout(function () {
+        changePicture(imagesGalleryArray, galleryId);  // Pass the arguments
+    }, slideTime);
 }
 
-// Call the function initially
-gallery1_ImgChange();
-
-// Automatically change the image every few seconds (e.g., every 5 seconds)
-setInterval(changeBackgroundImage, 5000);
+window.onload = function () {
+    changePicture(imagesGallery1, 'gallery1');  // Pass the arguments
+};
