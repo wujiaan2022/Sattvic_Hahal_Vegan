83adca73e8d51a6beb685b3f2fe70aaa35967913
wujiaan2022 <emmawu2004@gmail.com>
2023-08-12 13:51:38 +0000

    fixed slide not pause when mouse hover and not resume when mouse leave error

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,19 +1,15 @@
 
-var i = 0;
-
-var imagesGallery1 = [
-    '/static/images/colorfulDumplings0.jpg',
-    '/static/images/colorfulDumplings7.jpg', 
-    '/static/images/colorfulDumplings1.jpg',    
+var imagesGallery1 = [  
+    '/static/images/colorfulDumplings9.jpg',  
+    '/static/images/colorfulDumplings8.jpg',   
+    '/static/images/colorfulDumplings5.jpeg',  
+    '/static/images/colorfulDumplings7.jpg',        
     '/static/images/colorfulDumplings2.PNG',
     '/static/images/colorfulDumplings3.PNG',
-    '/static/images/colorfulDumplings4.jpeg',
-    '/static/images/colorfulDumplings5.jpeg',
-    '/static/images/colorfulDumplings6.jpg',      
-    '/static/images/colorfulDumplings8.jpg', 
-    '/static/images/colorfulDumplings9.jpg',
+    '/static/images/colorfulDumplings4.jpeg',    
+    '/static/images/colorfulDumplings6.jpg',  
+    '/static/images/colorfulDumplings1.jpg', 
 ];
-
 
 var imagesGallery3 = [
     '/static/images/sattvicVegan2.jpeg',
@@ -29,32 +25,70 @@ var imagesGallery3 = [
 var slideTime = 3000;
 var fadeTime = 1000;
 
-
-function changePicture(imagesGalleryArray, galleryId) {
-   
+function createGallery(imagesGalleryArray, galleryId, textId) {
+    var i = 0;
     var gallery = document.getElementById(galleryId);
-    
-    gallery.style.opacity = '0'; // Start with opacity 0      
+    var text = document.getElementById(textId);
+    var isHovered = false; // Track hover state
 
-    setTimeout(function () {
-         
-        gallery.style.backgroundImage = "url(" + imagesGalleryArray[i] + ")";
-        gallery.style.opacity = '1'; // Fade in by setting opacity to 1
-        if (i < imagesGalleryArray.length - 1) {
-            i++;
-        } else {
-            i = 0;
+    var changePictureTimeout;
+    var nextSlideTimeout;
+
+    function showText() {
+        text.style.opacity = '1';
+    }
+    
+    function hideText() {
+        text.style.opacity = '0';
+    }
+    
+    function changePicture() {
+        if (!isHovered) {
+            gallery.style.opacity = '0';
+            
+            changePictureTimeout = setTimeout(function () {
+                gallery.style.backgroundImage = "url(" + imagesGalleryArray[i] + ")";
+                gallery.style.opacity = '1';
+                
+                if (i < imagesGalleryArray.length - 1) {
+                    i++;
+                } else {
+                    i = 0;
+                }
+                
+                nextSlideTimeout = setTimeout(function () {
+                    changePicture();
+                }, slideTime - fadeTime);
+
+            }, fadeTime);
         }
-        
-       setTimeout(function () {
-            changePicture(imagesGalleryArray, galleryId);
-        }, slideTime-fadeTime);    
-    }, fadeTime); 
-}       
+    }
+
+    function startSlide() {
+        if (!isHovered) {
+            changePicture();
+        } else {
+            showText();
+        }
+    }
+
+    startSlide();
+
+gallery.addEventListener('mouseenter', function () {
+    isHovered = true;
+    showText();
+});
+    
+gallery.addEventListener('mouseleave', function () {
+    isHovered = false;
+    hideText();
+    clearTimeout(changePictureTimeout);
+    clearTimeout(nextSlideTimeout);
+    startSlide();
+});
+}
 
 window.onload = function () {
-    changePicture(imagesGallery1, 'gallery1');
-    changePicture(imagesGallery3, 'gallery3');
+    createGallery(imagesGallery1, 'gallery1', 'gallery1-text');
+    createGallery(imagesGallery3, 'gallery3', 'gallery3-text');
 };
-
-
