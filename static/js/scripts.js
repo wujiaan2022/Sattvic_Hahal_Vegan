868062f9b74212384e5fa00e137a9b884f1e67ab
wujiaan2022 <emmawu2004@gmail.com>
2023-08-13 09:50:07 +0000

    tidied up js code for auto bgChange and pause when hover

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -25,70 +25,51 @@ var imagesGallery3 = [
 var slideTime = 3000;
 var fadeTime = 1000;
 
-function createGallery(imagesGalleryArray, galleryId, textId) {
+function createGallery(imagesGalleryArray, galleryId) {
     var i = 0;
     var gallery = document.getElementById(galleryId);
-    var text = document.getElementById(textId);
-    var isHovered = false; // Track hover state
-
-    var changePictureTimeout;
+        
+    var showPictureTimeout;
     var nextSlideTimeout;
 
-    function showText() {
-        text.style.opacity = '1';
-    }
-    
-    function hideText() {
-        text.style.opacity = '0';
-    }
-    
+          
     function changePicture() {
-        if (!isHovered) {
-            gallery.style.opacity = '0';
+        
+        gallery.style.opacity = '0';
+        
+        showPictureTimeout = setTimeout(function () {
+            gallery.style.backgroundImage = "url(" + imagesGalleryArray[i] + ")";
+            gallery.style.opacity = '1';
             
-            changePictureTimeout = setTimeout(function () {
-                gallery.style.backgroundImage = "url(" + imagesGalleryArray[i] + ")";
-                gallery.style.opacity = '1';
-                
-                if (i < imagesGalleryArray.length - 1) {
-                    i++;
-                } else {
-                    i = 0;
-                }
-                
-                nextSlideTimeout = setTimeout(function () {
-                    changePicture();
-                }, slideTime - fadeTime);
+            if (i < imagesGalleryArray.length - 1) {
+                i++;
+            } else {
+                i = 0;
+            }
+            
+            nextSlideTimeout = setTimeout(function () {
+                changePicture();
+            }, slideTime - fadeTime);
 
-            }, fadeTime);
-        }
+        }, fadeTime);
+        
     }
 
-    function startSlide() {
-        if (!isHovered) {
-            changePicture();
-        } else {
-            showText();
-        }
-    }
-
-    startSlide();
+    changePicture();
 
 gallery.addEventListener('mouseenter', function () {
-    isHovered = true;
-    showText();
+    
+    clearTimeout(showPictureTimeout);
+    clearTimeout(nextSlideTimeout);
 });
     
 gallery.addEventListener('mouseleave', function () {
-    isHovered = false;
-    hideText();
-    clearTimeout(changePictureTimeout);
-    clearTimeout(nextSlideTimeout);
-    startSlide();
+            
+    changePicture();
 });
 }
 
 window.onload = function () {
-    createGallery(imagesGallery1, 'gallery1', 'gallery1-text');
-    createGallery(imagesGallery3, 'gallery3', 'gallery3-text');
+    createGallery(imagesGallery1, 'gallery1');
+    createGallery(imagesGallery3, 'gallery3');
 };
