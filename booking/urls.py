521f26a44afaec7e242f4edf14d45ce37a2042f7
wujiaan2022 <emmawu2004@gmail.com>
2023-08-26 15:43:46 +0000

    tried to fix confirmation page not showing up issue

--- booking/urls.py
+++ booking/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.check_available, name='check_available'),
     path('show/', views.show_available, name='show_available'),
     path('booking/<int:table_id>/<str:booking_date>/<str:booking_time>/', views.booking_form, name='booking_form'),
-#     path('booking_confirmation/<int:table_id>/<str:booking_date>/<str:booking_time>/', views.booking_confirmation, name='booking_confirmation'),
+    path('confirmation/<int:table_id>/<str:booking_date>/<str:booking_time>/', views.booking_confirmation, name='booking_confirmation'),
 ]
 
--- booking/views.py
+++ booking/views.py
@@ -17,65 +17,72 @@ def show_available(request):
     available_tables = []
 
     if booking_date and booking_time:
-        booked_table_numbers = Booking.objects.filter(date=booking_date, time=booking_time).values_list('table_id', flat=True)
-        available_tables = Table.objects.exclude(id__in=booked_table_numbers)
+        booked_table = Booking.objects.filter(date=booking_date, time=booking_time).values_list('table_id', flat=True)
+        available_tables = Table.objects.exclude(id__in=booked_table)
         
     return render(request, 'booking/show_available.html', {
-                'available_tables': available_tables,
-                
+                'available_tables': available_tables,                
                 'booking_date': booking_date,
                 'booking_time': booking_time,
                 }) 
   
 
 def booking_form(request, table_id, booking_date, booking_time):
-    
     table = Table.objects.get(id=table_id)
-    
-    # initial_data = {'table': table}
-    
-    # booking_date = datetime.strptime(booking_date, '%Y-%m-%d').date()
-    # initial_data = {'date': booking_date}
-    
-    # selected_time = None
-    # for choice_value, choice_label in Booking.TIME_CHOICES:
-    #     if choice_label == booking_time:
-    #         selected_time = choice_value
-    #         break
+    initial_data = {'table': table, 'date': booking_date, 'time': booking_time}
 
-    # if selected_time:
-    #     initial_data = {'time': selected_time}
-    
     if request.user.is_authenticated:
-        user = request.user
-        initial_data = {'name': user.username, 'email': user.email, 'table': table, 'date': booking_date, 'time':booking_time}
-    # else:
-    #     initial_data = {}
-
-    if request.method == 'POST':
-        print("Form submitted!")
-        form = BookingForm(request.POST)
+        initial_data['name'] = request.user.username
+        initial_data['email'] = request.user.email        
         
-        if form.is_valid():
-            booking = form.save(commit=False)
-            booking.user = request.user
-            booking.date = booking_date
-            booking.time = booking_time
+        if request.method == 'POST':
+            form = BookingForm(request.POST)
             
-            try:
-                booking.save()
-                print('Booking successful.')
-            except Exception as e:
-                print("Error while saving booking:", e)
+            if form.is_valid():
+                booking = form.save(commit=False)
+                booking.user = request.user
+                booking.date = booking_date
+                booking.time = booking_time
+                booking.name = request.user.username                 
                 
-            return redirect('home', table_id=table.id, booking_date=booking_date, booking_time=booking_time)
+                try:
+                    booking.save()
+                    print('Booking successful.')
+                except Exception as e:
+                    print("Error while saving booking:", e)
+                
+                return redirect('booking_confirmation', table_id=table.id, booking_date=booking_date, booking_time=booking_time,
+                                name=request.user.username, email=request.user.email)
+            else:
+                print("Form is NOT valid:", form.errors.as_data())
         else:
-            print("Form is NOT valid:", form.errors.as_data())
+            form = BookingForm(initial=initial_data)
     else:
-        form = BookingForm(initial=initial_data)
+        if request.method == 'POST':
+            form = BookingForm(request.POST)
+            if form.is_valid():
+                booking = form.save(commit=False)
+                booking.date = booking_date
+                booking.time = booking_time
+                booking.name = form.cleaned_data['name']
+                booking.email = form.cleaned_data['email']  
+                booking.phone = form.cleaned_data['phone']    
+                
+                try:
+                    booking.save()
+                    print('Booking successful.')
+                except Exception as e:
+                    print("Error while saving booking:", e)
+                
+                return redirect('booking_confirmation', table_id=table.id, booking_date=booking_date, booking_time=booking_time,
+                                name=booking.name, email=booking.email, phone=booking.phone)
     
-    return render(request, 'booking/booking_form.html', 
-                  {'table': table, 'form': form, 'booking_date': booking_date, 'booking_time': booking_time})
+    return render(request, 'booking/booking_form.html', {'form': form, 'table': table, 'booking_date': booking_date, 'booking_time': booking_time})
+
+
+    
+
+    
 
 
 def booking_confirmation(request, table_id, booking_date, booking_time):
@@ -84,6 +91,9 @@ def booking_confirmation(request, table_id, booking_date, booking_time):
         'table': table,
         'date': booking_date,
         'time': booking_time,
-        # Add other booking information here
+        'name': name,
+        'email': email,
+        'phone': phone,
+        
     }
-    return render(request, 'booking/booking_confirmation.html', {'booking': booking})        
+    return render(request, 'booking/confirmation.html', {'booking': booking})        
