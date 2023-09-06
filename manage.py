7cf8de2bd495fa31418a7b3736bd671292371658
wujiaan2022 <emmawu2004@gmail.com>
2023-09-06 11:13:45 +0000

    Renamed typo hahal to corect halal in all references

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sattvic_Hahal_Vegan.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sattvic_Halal_Vegan.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
